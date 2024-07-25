b26d89be7831cf3b01bd428832b830e03c45dd44
Bernhard M. Wiedemann <githubbmwprimary@lsmod.de>
2024-07-26 01:40:46 +0200

    Avoid storing build time in gzip headers (#13)
    Co-authored-by: James Addison <jay@jp-hosting.net>

--- tests/test_devhelp.py
+++ tests/test_devhelp.py
@@ -1,4 +1,5 @@
 """Test for devhelp extension."""
+from time import sleep
 
 import pytest
 
@@ -6,3 +7,19 @@ import pytest
 @pytest.mark.sphinx('devhelp', testroot='basic')
 def test_basic(app, status, warning):
     app.builder.build_all()
+
+
+@pytest.mark.sphinx('devhelp', testroot='basic', freshenv=True)
+def test_basic_deterministic_build(app):
+    app.config.devhelp_basename, output_filename = 'testing', 'testing.devhelp.gz'
+
+    app.builder.build_all()
+    output_initial = (app.outdir / output_filename).read_bytes()
+
+    sleep(2)
+
+    app.builder.build_all()
+    output_repeat = (app.outdir / output_filename).read_bytes()
+
+    msg = f"Content of '{output_filename}' differed between builds."
+    assert output_repeat == output_initial, msg
